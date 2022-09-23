dddb8e453305b0ce96b744d3596e0569013b141c
Sergey Osokin <saosokin@gmail.com>
2022-09-23 10:39:32 +0300

    the solution to the second task was added

--- seminar1/task2.py
+++ seminar1/task2.py
@@ -4,7 +4,7 @@ input_value=[[False,False,False],[False,False,True],[False,True,True],[True,True
 for i in input_value:
     left_part=not(i[0] or i[1] or i[2])
     right_part= not i[0] and not i[1] and not i[2] 
-    print(f'¬({i[0]} ⋁ {i[1]} ⋁ {i[2]})=¬{i[0]} ⋀ ¬{i[1]} ⋀ ¬{i[2]} -> {left_part==right_part}')
+    print(f'¬({i[0]} ⋁ {i[1]} ⋁ {i[2]})=¬{i[0]} ⋀ ¬{i[1]} ⋀ ¬{i[2]}  ->  {left_part==right_part}')
     
 
 
